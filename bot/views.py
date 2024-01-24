f77f0f41310cf2c330a320638313398f857ea008
Daniel Oliveira <devdan1337@gmail.com>
2024-01-24 11:14:20 -0300

    update-msg

--- bot/views.py
+++ bot/views.py
@@ -5,22 +5,24 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 from twilio.twiml.messaging_response import MessagingResponse
 from datetime import datetime, timedelta as td
+from unidecode import unidecode
+import re
 
 @csrf_exempt
 def bot(request):   
-    user_message = request.POST.get('Body', '').lower()
+    user_message = unidecode(request.POST.get('Body', ''))
 
     # Lista inicial de horários disponíveis
     horarios_disponiveis = gerar_horarios_disponiveis()
 
     # Verificar a mensagem do usuário e gerar a resposta apropriada
-    if user_message == 'horarios' or user_message == 'Horários' or user_message == 'Horarios' or user_message == 'horario':
+    if re.search(r'horari[oa]s?', user_message, re.IGNORECASE):
         response_message = f"Horários disponíveis:\n{formatar_horarios(horarios_disponiveis)}"
     elif user_message.startswith('reservar'):
         horario_reservado = user_message.replace('reservar ', '').strip()
         response_message = reservar_horario(horario_reservado, horarios_disponiveis.copy())
     else:
-        response_message = 'Bem vindo ao ChatBot da Barbearia. Digite "Horários" para ver os horários disponíveis.'
+        response_message = 'Bem-vindo ao ChatBot da Barbearia. Digite "Horários" para ver os horários disponíveis.'
 
     # Construir a resposta
     twilio_response = MessagingResponse()
@@ -63,7 +65,7 @@ def reservar_horario(horario_reservado, horarios_disponiveis):
 def enviar_mensagem(destinatario, mensagem):
     # Configurar as credenciais do Twilio
     account_sid = 'ACb079459447497a6a92e585083c1d4c7a'
-    auth_token = '90b724ad785ee8827b157555ddc21189'
+    auth_token = '2b07f7c0995fceea65b5e5074ff6936e'
     client = Client(account_sid, auth_token)
 
     # Enviar a mensagem
